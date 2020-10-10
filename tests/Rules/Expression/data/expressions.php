8e901cb77fe224df83bb68500a2dcb7d207c91e3
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-10-10 12:22:09 +0300

    add rule disallow unsed expression

---
+++ tests/Rules/Expression/data/expressions.php
@@ -0,0 +1,34 @@
+<?php
+
+function ds()
+{
+    return 2 + 5;
+}
+
+ds();
+
+2 + 5;
+
+ds(2 + 5);
+
+$b = ds(2 + 5);
+
+$a = 2 + 5;
+
+$a++;
+
+function example() {
+    9 + 7;
+
+    print_r();
+}
+
+if ($a > 0) {
+    example($a);
+}
+
+print_r('some text');
+
+for ($i = 0; 2 + 5; $i++) {
+    
+}
