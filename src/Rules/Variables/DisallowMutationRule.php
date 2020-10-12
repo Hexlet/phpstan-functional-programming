56ca40eee84f80d56ed5b68cb3f5471be7ee49b4
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-10-12 17:27:22 +0300

    remove comments

--- src/Rules/Variables/DisallowMutationRule.php
+++ src/Rules/Variables/DisallowMutationRule.php
@@ -26,30 +26,6 @@ abstract class DisallowMutationRule implements Rule
             return [];
         }
 
-        // if (isset($node->expr->var)) {
-        //     return [];
-        // }
-
-        // var_dump($node);
-        // var_dump($node->var);
-        // if (in_array($node->var, $this->hash)) {
-        //     var_dump('yes');
-        // } else {
-        //     $this->hash[] = $node->var;
-        //     var_dump(spl_object_id($node->var));
-        // }
-
-        // var_dump($node->byRef);
-        // var_dump($node->var->name);
-        // var_dump($node->getAttribute('startLine'));
-        // $variableType = $scope->getVariableType($node->var->name);
-        // var_dump($variableType);
-        // $parent = $node->getAttribute('parent');
-        // if ($parent->hasAttribute('previous')) {
-        //     $previous = $parent->getAttribute('previous');
-        //     var_dump($previous->expr->var->getAttribute('next')->getAttribute('phpstan_cache_printer'));
-        // }
-        
         if (isset($node->var) && $scope->hasVariableType($node->var->name)->yes()) {
             return ['Forbid the use of mutating operators'];
         }
